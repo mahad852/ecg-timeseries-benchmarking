51cdbe39f9fafaa2fd32e427abf12d128a88b75b
Mahad <mahad852@gmail.com>
2024-06-09 10:51:23 +0500

    separating llama loading process from prediction/inference function

--- benchmark_llama.py
+++ benchmark_llama.py
@@ -29,51 +29,50 @@ num_iterations = 10
 
 indices = random.sample(range(len(ecg_dataset)), total_samples)
 
-def get_lag_llama_predictions(dataset, prediction_length, device, context_length=32, use_rope_scaling=False, num_samples=100):
-    ckpt = torch.load("lagllama_weights/lag-llama.ckpt", map_location=device) # Uses GPU since in this Colab we use a GPU.
-    estimator_args = ckpt["hyper_parameters"]["model_kwargs"]
+device = torch.device("cuda:0") # You can switch this to CPU or other GPUs if you'd like, depending on your environment
 
-    rope_scaling_arguments = {
-        "type": "linear",
-        "factor": max(1.0, (context_length + prediction_length) / estimator_args["context_length"]),
-    }
+ckpt = torch.load("lagllama_weights/lag-llama.ckpt", map_location=device) # Uses GPU since in this Colab we use a GPU.
+estimator_args = ckpt["hyper_parameters"]["model_kwargs"]
 
-    estimator = LagLlamaEstimator(
-        ckpt_path="lagllama_weights/lag-llama.ckpt",
-        prediction_length=prediction_length,
-        context_length=context_length, # Lag-Llama was trained with a context length of 32, but can work with any context length
+rope_scaling_arguments = {
+    "type": "linear",
+    "factor": max(1.0, (context_len + pred_len) / estimator_args["context_length"]),
+}
 
-        # estimator args
-        input_size=estimator_args["input_size"],
-        n_layer=estimator_args["n_layer"],
-        n_embd_per_head=estimator_args["n_embd_per_head"],
-        n_head=estimator_args["n_head"],
-        scaling=estimator_args["scaling"],
-        time_feat=estimator_args["time_feat"],
-        rope_scaling=rope_scaling_arguments if use_rope_scaling else None,
-        batch_size=batch_size,
-        num_parallel_samples=100,
-        device=device,
-    )
+estimator = LagLlamaEstimator(
+    ckpt_path="lagllama_weights/lag-llama.ckpt",
+    prediction_length=pred_len,
+    context_length=context_len, # Lag-Llama was trained with a context length of 32, but can work with any context length
 
-    lightning_module = estimator.create_lightning_module()
-    transformation = estimator.create_transformation()
-    predictor = estimator.create_predictor(transformation, lightning_module)
+    # estimator args
+    input_size=estimator_args["input_size"],
+    n_layer=estimator_args["n_layer"],
+    n_embd_per_head=estimator_args["n_embd_per_head"],
+    n_head=estimator_args["n_head"],
+    scaling=estimator_args["scaling"],
+    time_feat=estimator_args["time_feat"],
+    rope_scaling=None,
+    batch_size=batch_size,
+    num_parallel_samples=100,
+    device=device,
+)
 
-    start_time = time.time()
+lightning_module = estimator.create_lightning_module()
+transformation = estimator.create_transformation()
+predictor = estimator.create_predictor(transformation, lightning_module)
 
+
+def get_lag_llama_predictions(dataset, num_samples=100):
     forecast_it, ts_it = make_evaluation_predictions(
         dataset=dataset,
         predictor=predictor,
         num_samples=num_samples
     )
 
-    end_time = time.time()
-
     forecasts = list(forecast_it)
     tss = list(ts_it)
 
-    return forecasts, tss, end_time - start_time
+    return forecasts, tss
 
 
 total_times = []
@@ -106,15 +105,15 @@ def build_timeseries(dataset: ECG_MIT, indices: list[int]):
 dataset = build_timeseries(ecg_dataset, indices)
 
 backtest_dataset = dataset
-prediction_length = pred_len  # Define your prediction length. We use 24 here since the data is of hourly frequency
-num_samples = 20 # number of samples sampled from the probability distribution for each timestep
-device = torch.device("cuda:0") # You can switch this to CPU or other GPUs if you'd like, depending on your environment
+num_samples = 100 # number of samples sampled from the probability distribution for each timestep
 
 
 for i in range(num_iterations):
-    _, _, time_taken = get_lag_llama_predictions(backtest_dataset, prediction_length, device, context_length=context_len, num_samples=num_samples)
+    start_time = time.time()
+    _, _, time_taken = get_lag_llama_predictions(backtest_dataset, num_samples=num_samples)
+    end_time = time.time()
 
-    print("Iteration:", i, "time taken:", time_taken)
+    print("Iteration:", i, "time taken:", end_time - start_time)
 
     total_times.append(time_taken)
 
