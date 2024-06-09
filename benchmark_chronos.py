562e8a29a9e125100d0c461179da8e5b4ff12569
Mahad <mahad852@gmail.com>
2024-06-09 09:51:27 +0500

    testing large chronos

--- benchmark_chronos.py
+++ benchmark_chronos.py
@@ -36,7 +36,7 @@ def batch_loader(dataset: ECG_MIT, indices: list[int], batch_size: int):
 
 indices = random.sample(range(len(ecg_dataset)), total_samples)
 
-model_type = "base"
+model_type = "large"
 
 pipeline = ChronosPipeline.from_pretrained(
     f"amazon/chronos-t5-{model_type}",
