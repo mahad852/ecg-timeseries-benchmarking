172c797eb1046abc5d8a4461928a4e056a374f81
Mahad <mahad852@gmail.com>
2024-06-09 08:30:48 +0500

    attempt to change tokenizer device

--- benchmark_chronos.py
+++ benchmark_chronos.py
@@ -40,9 +40,11 @@ pipeline = ChronosPipeline.from_pretrained(
     "amazon/chronos-t5-small",
     device_map="cuda:0",  # use "cpu" for CPU inference and "mps" for Apple Silicon
     torch_dtype=torch.float64,
+    
+    tokenizer_kwargs = {"device" : torch.device("cuda:0")},
 )
 
-print(pipeline.model.device)
+print(pipeline.model.device, pipeline.tokeizer.centers.device)
 
 
 total_times = []
